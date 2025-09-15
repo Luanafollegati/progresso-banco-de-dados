2cdd969be7b11217a82e9de3a65722c76b1b21e0
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-09-15 10:41:47 -0300

    feat: cria o arquivo para selects

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -11,5 +11,12 @@ CREATE TABLE artistas (
 );
 
 INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo)
+
 VALUES
 ('Now United', 'Banda', '14', 'Pop', 'Estados Unidos', 2017, 'Summer in the City', TRUE);
+
+('Stray Kids', 'Banda', '8', 'K-pop', 'Coréia do Sul', 2017, 'Hellevator', TRUE);
+
+('Arctic Monkeys', 'Banda', '4', 'Rock Alternativo', 'Reino Unido', 2002, 'I Bet You Look Good on the Dancefloor', TRUE);
+
+('Imagine Dragons', 'Banda', '4', 'Pop Rock', 'Estados Unidos', 2008, 'It s Time', TRUE )
