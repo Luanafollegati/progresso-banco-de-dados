35e196eaeb94127beb502b32ddb2fd5b47d58b2a
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-09-15 10:51:48 -0300

    feat: finaliza select

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,3 +1,15 @@
 SELECT * FROM artistas;
 
 SELECT COUNT(*) As  total_artistas FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+WHERE pais_origem = 'Brasil';
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+WHERE pais_origem = 'Estados Unidos';
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+WHERE id = 3;
+
