f499c942145297a7bce1aba7153bf20db3e05d89
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-09-29 11:40:19 -0300

    feat: cria pasta testes e arquivo acentos.sql

---
+++ testes/acentos.sql
@@ -0,0 +1,15 @@
+CREATE TABLE testes (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes (nome)
+VALUES
+('joão'),
+('maría'),
+('stéphanie'),
+('júlia'),
+('mário'),
+('vinícius'),
+('luís');
+
